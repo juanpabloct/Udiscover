eaa2b18ff77db863706b5840fa65138372b2a367
Juan Pablo Castellanos Torres <juanptorres2003@gmail.com>
2022-05-28 14:11:35 -0500

    Updates

--- src/components/Label.jsx
+++
@@ -1,5 +0,0 @@
-export default function Label({label, textLabel}) {
-    return (
-        <label className="text-white" htmlFor={label}>{textLabel}<span className="text-red-800 text-base">*</span></label>
-    )
-}
